d80da382a3d7fec015b28641cf1fdef528661200
akshatsamdani <akshat@zignex.com>
2024-06-18 10:56:56 +0530

    check

---
+++ check.ts
@@ -0,0 +1,18 @@
+interface User {
+    name:
+    string;
+    age: number;
+  }
+
+
+const user: User = {
+    name: 'Hayes',
+    age:
+     34
+
+};
+const user2: User;
+const user3: User;
+const user4: User;
+
+
