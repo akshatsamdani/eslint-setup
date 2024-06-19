cef79a1e36d84bf4add89e7ad9a40b0f7a194874
akshatsamdani <akshat@zignex.com>
2024-06-19 11:56:40 +0530

    demo

--- app.ts
+++ app.ts
@@ -1,12 +1,9 @@
 interface User {
-    name: string;
-    age: number;
-  }
+  name: string;
+  age: number;
+}
 
 const user5: User = {
-    name: 'Hayes',
-    age: 34
+  name: "Hayes",
+  age: 34,
 };
-
-console.log('user', user5);
-
